6a408efe333fa96281669d3ab60ea5c51bb84643
skiym-aaa <y.sakiyama3216@gmail.com>
2020-03-09 08:20:43 +0900

    [Finish] rails 5

---
+++ rails5_study_5/tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'次は何の本を読もうかな。','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'今日のランチおいしかった。','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(4,'たまには復習もしようかな！！','2020-03-07 09:08:57.041049','2020-03-07 09:12:29.932445');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',4);
+COMMIT;
