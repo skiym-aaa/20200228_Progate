cea908c9b41d4d3e693a6726456c39de94eddabd
skiym-aaa <y.sakiyama3216@gmail.com>
2020-07-03 20:31:53 +0900

    [Finish] Java 5

---
+++ java_study_5/page2/Bicycle.java
@@ -0,0 +1,4 @@
+// Vehicleクラスを継承してください
+class Bicycle extends Vehicle {
+  // 以下のコードをVehicleクラスに移動してください
+}
---
+++ java_study_5/page2/Car.java
@@ -0,0 +1,4 @@
+// Vehicleクラスを継承してください
+class Car extends Vehicle {
+  // 以下のコードをVehicleクラスに移動してください
+}
