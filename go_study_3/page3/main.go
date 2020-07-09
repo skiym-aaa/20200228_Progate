7c9405a318254231028a0486640d5fa43473fa6b
skiym-aaa <y.sakiyama3216@gmail.com>
2020-07-09 17:05:14 +0900

    [Finish] Go 2,3,4

---
+++ go_study_3/page3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 以下のコードを切り取ってください
+	
+	// ここから上を切り取ってください
+	
+	// ask関数を呼び出してください
+	ask()
+	
+}
+
+// ask関数を定義してください
+func ask() {
+    var input string
+	fmt.Println("次の単語を入力してください: dog")
+	fmt.Scan(&input)
+	fmt.Printf("%sと入力されました", input)
+}
